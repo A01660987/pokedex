3437f31933c86a30b1b8d8482376c4b3ddda18d1
Diego Figueroa Peart <a01660987@tec.mx>
2024-02-21 19:54:33 -0600

    pokedex background

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import PokedexBG from "./assets/pokedex-bg.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="absolute left-1/4" src={PokedexBG} alt="Pokedex"></img>
     </div>
   );
 }
